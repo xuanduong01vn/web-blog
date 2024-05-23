d5036f307e7d58b1bf532403a719bf8f34a97fe2
xuanduong01vn <xuanduong01vn@gmail.com>
2024-05-24 02:12:12 +0700

    Redesign header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,24 +1,40 @@
 import './HeaderStyle.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass,
+          faPen,
+          faBell
+ } from '@fortawesome/free-solid-svg-icons';
 
 function Header(){
     return (    
         <React.Fragment>
           <div className="header-container">
             <div className="header-bar">
-              <h1>BlogZ</h1>
+              <h1 className="header-title">BlogZ</h1>
               <div id="search-container">
-                <input type="text" />
+                <input id="search-box" type="text" placeholder='Tìm kiếm trên BlogZ'/>
                 <button id="search-btn">
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
                 </button>
               </div> 
-              <div id="user-container">
-                <a href="" id="signup-btn">
+              <div id="sign-container">
+                <a href="" id="sign-btn">
                   Đăng nhập/ Đăng ký
                 </a>
+              </div>
+              <div className="user-container">
+                <button className="new-blog-btn user-btn">
+                  <FontAwesomeIcon icon={faPen} className='new-blog-icon user-container-icon'/>
+                </button>
+                <button className="notify-btn user-btn">
+                  <FontAwesomeIcon icon={faBell} className='notify-icon user-container-icon'/>
+                </button> 
+                <button className="user-bar user-btn">
+                  <img src="https://www.vietnamfineart.com.vn/wp-content/uploads/2023/07/anh-avatar-dep-cho-con-gai-1.jpg" 
+                  alt="user avatar" className="user-image"/>
+                  <p className="user-name">username</p>
+                </button>
               </div>
             </div>
           </div>                                    
