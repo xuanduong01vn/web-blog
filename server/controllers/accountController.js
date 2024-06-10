c5821b3d53c956837f37a8a137fc475bba5973dd
xuanduong01vn <xuanduong01vn@gmail.com>
2024-06-10 10:44:57 +0700

    BE - add function add post, add accoutn trong controller

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,18 +1,54 @@
 import accountModel from '../models/accountModel.mjs';
-
 import { APIfeatures } from '../lib/features.js';
 
 const accountController ={
-  getPosts: async (req,res) =>{
+  getAccounts: async (req,res) =>{
     try{
-      let features = new APIfeatures(accountModel.find(), req.query);
+      let features = new APIfeatures(accountModel.find(), req.query)
+        .sorting()
+        .searching()
+        .filtering();;
       const allAccounts = await features.query;
       res.status(200).json(allAccounts);
     }
     catch(err){
       res.status(500).json(err.message);
     }
-  }
+  },
+  getAccount: async (req,res) =>{
+    try {
+      const acc = await accountModel.findById(req.params.id);
+      res.status(200).json(acc);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
+  addAccount: async (req, res) => {
+    try {
+      const { username, password, email } = req.body;
+
+      if (!username || !password || username.length==0 || !email ) {
+        return res.status(400).json("Username and password are required");
+      }
+
+      const newAccount = await accountModel(req.body);
+      const account = await accountModel.findOne({username});
+      const emailAccount = await accountModel.findOne({email});
+
+      if( account){
+        res.status(200).json("Tài khoản đã tồn tại");
+      }
+      else if(emailAccount){
+        res.status(200).json("Email đã tồn tại");
+      }
+      else{
+        const saveAccount = await newAccount.save();
+        res.status(200).json(saveAccount);
+      }
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
 };
 
 export default accountController;
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -5,7 +5,10 @@ import { APIfeatures } from '../lib/features.js';
 const postController ={
   getPosts: async (req,res) =>{
     try{
-      let features = new APIfeatures(postModel.find(), req.query);
+      let features = new APIfeatures(postModel.find(), req.query)
+        .sorting()
+        .searching()
+        .filtering();
 
       const allPosts = await features.query;
 
@@ -14,7 +17,41 @@ const postController ={
     catch(err){
       res.status(500).json(err.message);
     }
-  }
+  },
+  getPost: async (req,res) =>{
+    try {
+      const post = await postModel.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
+  addPost: async (req, res) => {
+    try {
+      const newPost = await postModel(req.body);
+      const savePost = await newPost.save();
+      res.status(200).json(savePost);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
+  updatePost: async (req, res) => {
+    try {
+      const post = await postModel.findById(req.params.id);
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Updated successfully!");
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
+  deletePost: async (req, res) => {
+    try {
+      const post = await postModel.findByIdAndDelete(req.params.id);
+      res.status(200).json("Deleted successfully!");
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
 };
 
 export default postController;
