1e5955aee93d06a7337a4bb608ae8d92dc99fe69
xuanduong01vn <xuanduong01vn@gmail.com>
2024-06-10 10:43:31 +0700

    FE - hoan thien post data admin moi

--- client/src/components/DashboardAdmin/DashboardNewAdmin.js
+++ client/src/components/DashboardAdmin/DashboardNewAdmin.js
@@ -1,59 +1,152 @@
 import styled from 'styled-components';
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera
-} from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 function NewAdmin(){
+
+  const [inputUsername, setInputUsername]=useState("");
+  const [inputEmail, setInputEmail]=useState("@gmail.com");
+  const [inputPassword, setInputPassword]=useState("");
+  const [confirmPassword, setConfirmPassword]=useState("");
+  const [samePassword, setSamePassword] = useState(null);
+  const [sameAccount, setSameAccount] = useState(false);
+  const [adminList, setAdminList] = useState(null);
+  const [success, setSuccess]= useState(false);
+  const [legitPass, setLegitPass] = useState(true);
+
+  var [inputValue, setInputValue] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  useEffect(()=>{
+    const getDataAdmin = async () => {
+      try {
+        const response = await axios.get("http://localhost:9999/accounts/");
+        return response.data;
+      } catch (err) {
+        console.log("Error fetching authors:", err.message);
+        return [];
+      }
+    };
+    getDataAdmin()
+    .then((data) => {
+      setAdminList(data || []);
+    })
+    .catch((err)=>{
+      console.log(err.message);
+    });
+  },[]);
+
+  function onChangeValue(e, func){
+    const {name, value} = e.target;
+      if(e.target.name == "username"){
+        setInputEmail(e.target.value.trim()+`@gmail.com`);
+        setSameAccount(adminList?.some(admin=>admin.username==e.target.value.trim()));
+      }
+      if(e.target.name=="password"){
+        setLegitPass(e.target.value.trim().length>=6);
+      }
+      setInputValue({
+        ...inputValue,
+        [name]: value,
+      });
+      func(e.target.value.trim());  
+      
+  }
+
+  function clearInput(){
+    setInputValue({
+      username: '',
+      password: '',
+      confirmPassword: '',
+    })
+  };
+
+  var nowTime = new Date();
+  var createAt;
+
+  var newInputAccount={
+    username: inputUsername,
+    password: inputPassword,
+    email: inputEmail,
+    fullname: "",
+    idTypeAccount: 1,
+    birthday: "",
+    createAt: "createAt",
+    avatar: "",
+    listLiked: [],
+    listMarked: [],
+    isDeleted: false
+  };
+
+
+  function addNewAdmin(){
+    createAt = `${nowTime.getHours()+7}:${nowTime.getMinutes()} ${nowTime.getDate()}/${nowTime.getMonth()+1}/${nowTime.getFullYear()}`;
+    newInputAccount.createAt=createAt;
+    setSamePassword(confirmPassword != inputPassword);
+    if(inputPassword.length>=6 && confirmPassword !="" && confirmPassword == inputPassword && !sameAccount){
+      axios.post(`http://localhost:9999/accounts/`,newInputAccount)
+      .then(res=>{
+        console.log(res.data);
+        clearInput();
+        setInputEmail("@gmail.com");
+        setSuccess(true)
+        setTimeout(()=>setSuccess(false),2000);
+      })
+      
+    }
+    
+  }
+
   return(
     <Wrapper>
-      <h2 className="profile-container-title">Tạo mới quản trị viên</h2>
+      <h2 className="profile-container-title">Thêm mới quản trị viên</h2>
       <div className="profile-container">
-        <div className="profile-item">
-          <img src="https://www.vietnamfineart.com.vn/wp-content/uploads/2023/07/anh-avatar-dep-cho-con-gai-1.jpg"
-          alt="Ảnh đại diện" className="profile-avatar"/>
-          <button className="avatar-btn">
-            <FontAwesomeIcon icon={faCamera} className="avatar-btn-icon"/>
-          </button>
-        </div>
         <div className="profile-item">
           <label htmlFor="">
             <span className="red-asterisk">* </span>Tên tài khoản
           </label>
-          <input type="text" id="profile-username" className="profile-input" value={"xuanduong"}/>
-        </div>
-        <div className="profile-item">
-          <label htmlFor="profile-fullname">
-            <span className="red-asterisk">* </span>Tên hiển thị
-          </label>
-          <input type="text" id="profile-fullname" className="profile-input" value={"xuanduong"}/>
+          <input name="username" type="text" id="profile-username" className="profile-input"
+          value={inputValue.username}
+          onChange = {e=> onChangeValue(e, setInputUsername)}/>
+          {sameAccount && (<span className="warning-box">Tên tài khoản này đã được sử dụng</span>)}
         </div>
         <div className="profile-item">
           <label htmlFor="profile-email">
           <span className="red-asterisk">* </span>Email
         </label>
-          <input type="text" id="profile-email" className="profile-input" value={"xuanduong"}/>
-        </div>
-        <div className="profile-item">
-          <label htmlFor="profile-birthday">
-          <span className="red-asterisk">* </span>Ngày sinh
-        </label>
-          <input type="text" id="profile-birthday" className="profile-input" value={"xuanduong"}/>
+          <input name="email" type="text" id="profile-email" className="profile-input read-only"
+          value={inputEmail} readOnly/>
         </div>
         <div className="profile-item">
           <label htmlFor="profile-birthday">
           <span className="red-asterisk">* </span>Mật khẩu
         </label>
-          <input type="text" id="profile-birthday" className="profile-input" value={"xuanduong"}/>
+          <input name="password" type="text" id="profile-birthday" className="profile-input"
+          value={inputValue.password}
+          onChange = {e=> onChangeValue(e, setInputPassword)}/>
+          {!legitPass && (<span className="warning-box">Mật khẩu phải có ít nhất 6 ký tự</span>)}
         </div>
         <div className="profile-item">
           <label htmlFor="profile-birthday">
           <span className="red-asterisk">* </span>Xác nhận lại mật khẩu
         </label>
-          <input type="text" id="profile-birthday" className="profile-input" value={"xuanduong"}/>
+          <input name="confirmPassword" type="text" id="profile-birthday" className="profile-input"
+          value={inputValue.confirmPassword}
+          onChange = {e=> onChangeValue(e, setConfirmPassword)}/>
+          {samePassword && (<span className="warning-box">Mật khẩu không khớp</span>)}
         </div>
+        <div className="profile-item">
+          {success && (<span className="success-alert">Thêm mới tài khoản thành công</span>)}
+        </div>
+        
+        
         <div className="profile-action">
-          <a href="/dashboard/admins" className="profile-btn active">Lưu</a>
+          <a onClick={addNewAdmin} 
+          // href="/dashboard/admins"
+           className="profile-btn active">Lưu</a>
         </div>
         
       </div>
@@ -81,6 +174,7 @@ const Wrapper = styled.div`
   .profile-item{
     position: relative;
     margin-bottom: 20px;
+    min-height: 22px;
   }
 
   .red-asterisk{
@@ -117,7 +211,20 @@ const Wrapper = styled.div`
     font-size: 18px;
   }
 
-  .profile-input{
+  .read-only{
+    color: var(--text-color);
+
+    &:focus{
+      border: 1px solid var(--shadow-color);
+    }
+  }
+
+  .warning-box{
+    position: absolute;
+    top:100%;
+    color: red;
+    display: block;
+    font-size: 14px;
   }
 
   .profile-action{
